af696beef0e71fb004072f9e8b81c8152196c12a
sapenagamero0 <sapenagamero0@frostburg.edu>
2016-08-06 20:35:49 -0500

    more changes

--- src/gittest/Gittest.java
+++ src/gittest/Gittest.java
@@ -15,6 +15,8 @@ public class Gittest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-System.out.println("hello");    }
+System.out.println("hello"); 
+    
+    System.out.println("more code");}
     
 }
