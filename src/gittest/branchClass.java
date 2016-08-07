e2ce3487a8fbaeff61ca063bfae4bd8986cbc022
sapenagamero0 <sapenagamero0@frostburg.edu>
2016-08-06 21:48:17 -0500

    branch changes

---
+++ src/gittest/branchClass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gittest;
+
+/**
+ *
+ * @author Saul Pena Gamero
+ */
+public class branchClass {
+ 
+    System.out.println("this is branch class");
+}
